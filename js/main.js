078ea7bd981adae98b930e1699d0af6691eeb45a
natfitzpatrick <NatalieFitzpatrick@003-GDMA-Mac.local>
2017-03-20 11:42:15 -0700

    Files

---
+++ js/main.js
@@ -0,0 +1,22 @@
+$('#buttonStart').click( function() {
+  $('#startScreen').addClass('test');
+});
+
+setTimeout(function() {
+  $('#startScreen').addClass('test');
+}, 5000)
+
+setTimeout(function() {
+  $('#startScreen').removeClass('test');
+}, 15000)
+
+$('.mainNav a').click( function(){
+    $('.hide').fadeOut(500);
+    $( $(this).attr('href') ).fadeIn(750);
+});
+
+$('.btn-close').click(
+  function(){
+    $(this).closest('.content-box').fadeOut(500);
+  }
+)
